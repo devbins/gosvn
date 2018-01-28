51fa51928944e024d4399ad4b7f8c6e729fc17a1
bin <bin6160@126.com>
2018-01-28 20:27:38 +0800

    svn info

--- client.go
+++ client.go
@@ -7,51 +7,66 @@ import (
 	"os/exec"
 )
 
-type Cmder interface {
-	// Run ...
-	Run(cmd string)
-}
-
-type client struct {
+type Client struct {
+	Username string
+	Password string
+	SvnUrl   string
+	SvnDir   string
 	Env      []string
-	svnUrl   string
-	svnDir   string
-	username string
-	password string
-}
-
-// Run ...
-func (this *client) Run(args ...string) (string, error) {
-	cmd := exec.Command("svn", args, "--username", this.username, "--password", this.password)
-	cmd.Env = append(os.Environ(), client.Env...)
-	out, err := cmd.Output()
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	return out, nil
 }
 
 // log ...
-func (this *client) log(cmd string) (logEntry, error) {
+func (this *Client) log(cmd string) (*logEntry, error) {
 	out, err := this.Run(cmd)
 	if err != nil {
 		return nil, err
 	}
 	log := new(logEntry)
-	err = xml.Unmarshal(out, log)
+	err = xml.Unmarshal([]byte(out), log)
 	if err != nil {
 		return nil, err
 	}
-
+	return log, nil
 }
 
 // checkout
-func (this *client) checkout(cmd string) (string, error) {
-
+func (this *Client) checkout(cmd string) (string, error) {
+	fmt.Println("checkout")
+	return "", nil
 }
 
 // client checkout from specific revision
-func (this *client) checkoutWithRevision(url string, revision int) (string, error) {
+func (this *Client) checkoutWithRevision(url string, revision int) (string, error) {
+	fmt.Println("checkoutwithrevision")
+	return "", nil
+}
 
+// info ...
+func (this *Client) Info() (string, error) {
+	cmd := "info"
+	out, err := this.Run(cmd)
+	if err != nil {
+		return "", err
+	}
+	info := new(info)
+	err = xml.Unmarshal([]byte(out), info)
+	if err != nil {
+		return "", err
+	}
+	return out, nil
+}
+
+// Run ...
+func (this *Client) Run(args ...string) (string, error) {
+	ops := []string{this.SvnUrl, "--xml", "--username", this.Username, "--password", this.Password}
+	args = append(args, ops...)
+	cmd := exec.Command("svn", args...)
+	cmd.Env = append(os.Environ(), this.Env...)
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Println("error")
+		fmt.Println(err)
+		return "", err
+	}
+	return string(out), nil
 }
