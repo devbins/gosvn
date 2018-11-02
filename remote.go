c36cad0812fef333012d3aa664e176a8012f170a
bingo <bin6160@126.com>
2018-11-02 15:48:17 +0800

    add remote

--- remote.go
+++ remote.go
@@ -1,7 +1,25 @@
 package gosvn
 
-
+import (
+	"os/exec"
+)
 
 type remoteClient struct {
-	
+	url      string
+	username string
+	password string
+}
+
+// CheckOut ...
+func (this *remoteClient) CheckOut(dir string) error {
+	args := []string{"checkout", this.url, dir, "--username", this.username, "--password", this.password}
+	_, err := runcmd(args...)
+	return err
+}
+
+// run ...
+func runcmd(args ...string) ([]byte, error) {
+	cmd := exec.Command("svn", args...)
+	out, err := cmd.Output()
+	return out, err
 }
