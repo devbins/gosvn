c472304ba0f8c0a32fb958c1ec623abf8b59c63e
bin <bin6160@126.com>
2018-11-24 20:56:18 +0800

    add checkout

--- remote.go
+++ remote.go
@@ -1,25 +1,23 @@
 package gosvn
 
-import (
-	"os/exec"
-)
-
 type remoteClient struct {
-	url      string
-	username string
-	password string
+	CommonClient
 }
 
 // CheckOut ...
-func (this *remoteClient) CheckOut(dir string) error {
-	args := []string{"checkout", this.url, dir, "--username", this.username, "--password", this.password}
-	_, err := runcmd(args...)
-	return err
+func (remoteClient *remoteClient) CheckOut(dir string) error {
+	_, err := remoteClient.RunCmd("checkout", dir, "--xml", remoteClient.URLOrPath)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-// run ...
-func runcmd(args ...string) ([]byte, error) {
-	cmd := exec.Command("svn", args...)
-	out, err := cmd.Output()
-	return out, err
+// CheckOutWithRevision ...
+func (remoteClient *remoteClient) CheckOutWithRevision(dir string, revision int) error {
+	_, err := remoteClient.RunCmd("checkout", dir, "-r", string(revision), "--xml", remoteClient.URLOrPath)
+	if err != nil {
+		return err
+	}
+	return nil
 }
